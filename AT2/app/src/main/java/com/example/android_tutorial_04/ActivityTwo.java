a3a1a5a39976fc4a4835d82503ff0bfddb336dc5
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-03 18:37:58 +0530

    Update ActivityTwo.java

--- AT2/app/src/main/java/com/example/android_tutorial_04/ActivityTwo.java
+++ AT2/app/src/main/java/com/example/android_tutorial_04/ActivityTwo.java
@@ -2,13 +2,28 @@ package com.example.android_tutorial_04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityTwo extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        button = findViewById(R.id.btn2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
