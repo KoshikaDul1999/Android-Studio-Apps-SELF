426f6657162636c56c17bf41862b5d2d79cd81a9
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-03 16:12:37 +0530

    Update MainActivity.java

--- AT1/app/src/main/java/com/example/android_tutorial1/MainActivity.java
+++ AT1/app/src/main/java/com/example/android_tutorial1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.android_tutorial1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnok);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = findViewById(R.id.txthello);
+                text.setText("Hai Koshika Dulanjana");
+            }
+        });
     }
 }
