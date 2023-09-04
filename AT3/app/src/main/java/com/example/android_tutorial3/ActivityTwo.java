07e130d1b5d4258c62d76c88fe620bebecee5896
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-04 23:27:51 +0530

    Update ActivityTwo.java to receive data

--- AT3/app/src/main/java/com/example/android_tutorial3/ActivityTwo.java
+++ AT3/app/src/main/java/com/example/android_tutorial3/ActivityTwo.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityTwo extends AppCompatActivity {
 
     Button button;
+    TextView textUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,12 @@ public class ActivityTwo extends AppCompatActivity {
         setContentView(R.layout.activity_two);
 
         button = findViewById(R.id.btn2);
+        textUserName = findViewById(R.id.textUserName);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("USER_NAME");
+
+        textUserName.setText(name);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
