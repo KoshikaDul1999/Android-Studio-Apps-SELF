9d5ee838497bc1f82773ccc676734ba3ae5fb259
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-06 16:36:37 +0530

    Update ManinActivity.java

--- AT5/app/src/main/java/com/example/android_tutorial5/MainActivity.java
+++ AT5/app/src/main/java/com/example/android_tutorial5/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_tutorial5;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,23 +18,32 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.btn1);
 
-        final Intent intent = new Intent(this,ActivityTwo.class);
-        String name = "Koshika Dulanjana";
-        int age = 24;
-
-//        intent.putExtra("USER_NAME",name);
-//        intent.putExtra("USER_AGE",age);
-        Bundle extras = new Bundle();
-        extras.putString("USER_NAME", name);
-        extras.putInt("USER_AGE", age);
-
-        intent.putExtras(extras);
+//        final Intent intent = new Intent(this,ActivityTwo.class);
+//        String name = "Koshika Dulanjana";
+//        int age = 24;
+//
+////        intent.putExtra("USER_NAME",name);
+////        intent.putExtra("USER_AGE",age);
+//        Bundle extras = new Bundle();
+//        extras.putString("USER_NAME", name);
+//        extras.putInt("USER_AGE", age);
+//
+//        intent.putExtras(extras);
+        
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        final Intent chooser;
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"test@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "This is our test subject");
+        intent.putExtra(Intent.EXTRA_TEXT, "This is our test email body");
+        intent.setType("message/rfc822");
+        intent.createChooser(intent, "Send email test app");
+        
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                startActivity(intent);
+                startActivity(chooser);
             }
         });
     }
