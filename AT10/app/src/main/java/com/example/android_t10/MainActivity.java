a3bc0bd5690cb8f1b0b6240951247f67fc5f8e3e
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-21 11:07:49 +0530

    Update MainActivity.java

--- AT10/app/src/main/java/com/example/android_t10/MainActivity.java
+++ AT10/app/src/main/java/com/example/android_t10/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android_t10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"Hello this is toast",Toast.LENGTH_LONG);
+        toast.show();
     }
 }
